6ae6383408eaa2b6757765efd929cf1b551a69a4
javie <javie@MSI>
2023-11-07 08:47:40 -0600

    Subir Proyecto

---
+++ src/test/java/com/proyecto_v1/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
