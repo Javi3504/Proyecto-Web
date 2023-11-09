7301adca5ec90d4fe4f613c2dd554c972385c7e6
Paula <Paula@DESKTOP-2C757CK>
2023-11-09 13:15:14 -0600

    Se modifica la clase Producto

--- src/main/java/com/proyecto/domain/Producto.java
+++ src/main/java/com/proyecto/domain/Producto.java
@@ -3,11 +3,107 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.proyecto.domain;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
 /**
  *
  * @author Paula
  */
-public class Producto {
-    
+
+
+@Data
+@Entity
+@Table(name = "producto")
+public class Producto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private long idProducto;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "ruta_imagen")
+    private String rutaImagen;
+
+    @Column(name = "activo")
+    private boolean activo;
+
+    public Producto() {
+    }
+
+    public Producto(String descripcion, boolean activo) {
+        this.descripcion = descripcion;
+        this.activo = activo;
+    }
+
+    // Getters y setters
+
+    public long getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(long idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getRutaImagen() {
+        return rutaImagen;
+    }
+
+    public void setRutaImagen(String rutaImagen) {
+        this.rutaImagen = rutaImagen;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    // Métodos toString(), equals() y hashCode()
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "idProducto=" + idProducto +
+                ", descripcion='" + descripcion + '\'' +
+                ", rutaImagen='" + rutaImagen + '\'' +
+                ", activo=" + activo +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Producto producto = (Producto) o;
+
+        return idProducto == producto.idProducto;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (idProducto ^ (idProducto >>> 32));
+    }
 }
