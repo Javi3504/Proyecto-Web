49c3d6842d228fe8128ea8f41ce28f65318c47da
Javi3504 <101346449+Javi3504@users.noreply.github.com>
2023-12-13 07:16:33 -0600

    Carrito.

---
+++ Proyecto/src/main/java/com/proyecto/dao/CategoriaDao.java
@@ -0,0 +1,12 @@
+package com.proyecto.dao;
+
+import com.proyecto.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author PC MASTER
+ */
+public interface CategoriaDao extends JpaRepository<Categoria, Long> {
+
+}
---
+++ Proyecto/src/main/java/com/proyecto/service/ReporteService.java
@@ -0,0 +1,18 @@
+package com.proyecto.service;
+
+
+import java.io.IOException;
+import java.util.Map;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+
+
+public interface ReporteService {
+    public ResponseEntity<Resource> generaReporte(
+    
+         String reporte, //Nombre del archivo jasper usuarios o ventas
+            Map<String, Object> parametros,
+                    String tipo) throws IOException;
+    
+    
+}
